2c597ef39923f2330471ce002700b12d180d4cec
Heisenth <Heisenth@gmail.com>
2022-09-27 09:40:42 +0200

    Index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('express');
+const express = require('express');
+const app = express();
+
+/*
+get "/"
+get "obtenerPartidas"
+post get "agregarUsuario/:nick"
+put "/actualizarPartida"
+delete "/eliminarPartida"
+*/
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send("Hola")
+    .end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+// [END gae_flex_quickstart]
