f1a7f05a96541dedb7f149e4ca4c21d23340489c
Heisenth <Heisenth@gmail.com>
2022-12-05 12:57:52 +0100

    Tablero css

--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -1,129 +1,199 @@
-function ControlWeb(){
-	this.comprobarCookie=function(){
-		if ($.cookie("nick")){
-			rest.nick=$.cookie("nick");
-			cws.conectar();
-			this.mostrarHome();
+function ControlWeb() {
+	this.recuperarSesion = function () {
+	  usuario = $.cookie("nick");
+	  if (!usuario) {
+		this.limpiarPantalla();
+		this.mostrarAgregarUsuario();
+		return;
+	  }
+	  rest.agregarUsuarioDesdeCookie(usuario);
+	};
+  
+	this.recuperarSesionCallback = function () {
+	  this.mostrarHome();
+	  partida = $.cookie("codigoP");
+	  if (partida) {
+		this.mostrarPartidaUnido(partida);
+	  }
+	};
+	this.mostrarAgregarUsuario = function () {
+	  var cadena = '<div id="mAU"><h2>Inicio de Sesión</h2><div class="row" >'; //'<form class="form-row needs-validation"  id="mAU">';
+	  cadena = cadena + '<div class="col"><h2>El juego indefinido</h2></div>';
+	  cadena = cadena + '<div class="row">';
+	  cadena = cadena + '<div class="row">';
+	  cadena =
+		cadena +
+		'<input type="text" class="form-control mb-2 mr-sm-2" id="usr" placeholder="Introduce tu nick (max 6 letras)" required></div>';
+	  cadena = cadena + '<div class="col">';
+	  cadena =
+		cadena +
+		'<button id="btnAU" class="btn btn-primary mb-2 mr-sm-2">Iniciar sesión</button>';
+	  //cadena=cadena+'<a href="/auth/google" class="btn btn-primary mb-2 mr-sm-2">Accede con Google</a>';
+	  cadena = cadena + "</div></div>"; //' </form>';
+	  cadena = cadena + '<div id="nota"></div></div>';
+  
+	  $("#agregarUsuario").append(cadena);
+	  //$("#nota").append("<div id='aviso' style='text-align:right'>Inicia sesión con Google para jugar</div>");
+  
+	  $("#btnAU").on("click", function (e) {
+		if ($("#usr").val() === "" || $("#usr").val().length > 6) {
+		  e.preventDefault();
+		  $("#nota").text("Nick inválido");
+		} else {
+		  var nick = $("#usr").val();
+		  $("#mAU").remove();
+		  $("#aviso").remove();
+		  rest.agregarUsuario(nick);
 		}
-		else{
-			this.mostrarAgregarUsuario();
-		}
-	}
-	this.mostrarAgregarUsuario=function(){
-		let cadena= '<div class="row" id="mAU">';//'<form class="form-row needs-validation"  id="mAJ">';
-		cadena=cadena+"<div class='col'>";
-		cadena=cadena+'<div class="row"><div class="col"><h2>El juego indefinido</h2></div></div>';
-		cadena=cadena+'<div class="row">';
-		cadena=cadena+'<div class="col">'
-        cadena=cadena+'<input type="text" class="form-control mb-2 mr-sm-2" id="usr" placeholder="Introduce tu nick (max 6 letras)" required></div>';
-        cadena=cadena+'<div class="col">';
-        cadena=cadena+'<button id="btnAU" class="btn btn-primary mb-2 mr-sm-2">Iniciar sesión</button>';
-        //cadena=cadena+'<a href="/auth/google" class="btn btn-primary mb-2 mr-sm-2">Accede con Google</a>';
-        cadena=cadena+'</div>'; //' </form>';
-        cadena=cadena+'<div id="nota"></div>';
-        cadena=cadena+'</div></div></div>';
-
-		$("#agregarUsuario").append(cadena);     
-		//$("#nota").append("<div id='aviso' style='text-align:right'>Inicia sesión con Google para jugar</div>");    
-
-		$("#btnAU").on("click",function(e){
-			if ($('#usr').val() === '' || $('#usr').val().length>6) {
-			    e.preventDefault();
-			    $('#nota').append('Nick inválido');
-			}
-			else{
-				var nick=$('#usr').val();
-				$("#mAU").remove();
-				//$("#aviso").remove();
-				rest.agregarUsuario(nick);
-				//mostrar gif
-			}
-		})
-	}
-	this.mostrarHome=function(){
-		$('#mH').remove();
-		let cadena="<div class='row' id='mH'>";
-		cadena=cadena+'<div class="col">';
-		cadena=cadena+"<p>Bienvenido "+rest.nick+"</p>";
-		cadena=cadena+'<button id="btnSalir" class="btn btn-primary mb-2 mr-sm-2">Salir</button>';
-		cadena=cadena+"<div id='codigo'></div>"
-		cadena=cadena+"</div></div>";
-		$('#agregarUsuario').append(cadena);
-		this.mostrarCrearPartida();
-		rest.obtenerListaPartidasDisponibles();
-		$("#btnSalir").on("click",function(e){		
-			$("#mCP").remove();
-			$('#mLP').remove();
-			$('#mH').remove();
-			//rest.crearPartida();
-			rest.usuarioSale();
-			//$.removeCookie("nick");
-			//iu.comprobarCookie();
+	  });
+	};
+  
+	this.mostrarHome = function () {
+	  var cli = this;
+  
+	  $("#mHin").remove();
+	  var cadena =
+		"<div class = 'row' id='mHin'>" +
+		"<div class='col'>" +
+		"<h2>Bienvenido " +
+		$.cookie("nick") +
+		"</h2>" +
+		"<div id='partida'>" +
+		"</div>" +
+		"</div>";
+	  $("#mH").append(cadena);
+	  this.mostrarCrearPartida();
+	  this.mostrarListaDePartidas();
+  
+	  var cadenaCerrarSesion =
+		"<button id='btnLO' class='btn btn-primary mb-2 mr-sm-2'>Cerrar Sesión</button>";
+	  $("#logOut").html(cadenaCerrarSesion);
+  
+	  $("#btnLO").on("click", function (e) {
+		cws.eliminarUsuario($.cookie("nick"));
+	  });
+	};
+  
+	this.mostrarCrearPartida = function () {
+	  //Dibujar un botón que al hacer click llame a crear partida
+	  $("mCP").remove();
+	  var cadena =
+		"<div class='row' id='mCP'>" +
+		"<div class='col'>" +
+		"<button id='btnAP' class='btn btn-primary mb-2 mr-sm-2'>Crear Partida</button>" +
+		"</div>" +
+		"</div>";
+	  $("#crearPartida").html(cadena);
+  
+	  $("#btnAP").on("click", function (e) {
+		cws.crearPartida($.cookie("nick"));
+	  });
+	};
+  
+	this.mostrarPartidaUnido = (codigo) => {
+	  if (!codigo || codigo === "null") return;
+  
+	  $("#mCP").remove();
+	  $("#cP").remove();
+	  $("#partida").html("");
+  
+	  let cadenaCodigo = "<div id='codigo'></div>";
+	  let cadenaBoton = "<div id='SP'></div>";
+	  $("#partida").append(cadenaCodigo);
+	  $("#partida").append(cadenaBoton);
+  
+	  this.mostrarCodigo(codigo);
+	  this.mostrarBotonSalirPartida(codigo);
+	};
+  
+	this.mostrarCodigo = function (codigo) {
+	  if (codigo) {
+		var cadena =
+		  "<div id='cP'><p> Código de la partida: " + codigo + "</p></div>";
+		$("#codigo").append(cadena);
+	  }
+	};
+  
+	this.mostrarBotonSalirPartida = function (codigo) {
+	  var cadena =
+		"<button  id='btnSP' class='btn btn-warning btn-labeled mb-2 mr-sm-2'>Salir Partida</button> ";
+  
+	  $("#SP").append(cadena);
+  
+	  $("#btnSP").on("click", function (e) {
+		cws.salirPartida($.cookie("nick"), codigo);
+		//Salir partida
+		console.log("Salir partida");
+	  });
+	};
+  
+	this.limpiarListaDePartidas = function () {
+	  $("#mLP").remove();
+	};
+  
+	this.mostrarListaDePartidas = function () {
+	  this.limpiarListaDePartidas();
+	  var cadena =
+		"<div class='col' id='mLP'>" +
+		"<button  id='btnRP' class='btn btn-info btn-labeled mb-2 mr-sm-2'><span class='btn-label'><i class='fa fa-refresh'></i></span> Refrescar</button> " +
+		"<div class='row'>" +
+		"<ul class='list-group' id='LP'></ul></div>" +
+		"</div>";
+  
+	  $("#listaPartidas").append(cadena);
+  
+	  $("#btnRP").on("click", function (e) {
+		// $("#mCP").remove();
+		$("#LP").html("");
+		rest.obtenerPartidasDisponibles();
+	  });
+  
+	  rest.obtenerPartidasDisponibles();
+	};
+  
+	this.mostrarListaDePartidasCallback = function (partidas) {
+	  $(".linea-partida").remove();
+  
+	  for (partida of partidas) {
+		var cadena =
+		  "<li class='linea-partida list-group-item'><span>" +
+		  "Código: " +
+		  partida.codigo +
+		  "</span>  " +
+		  "<span>" +
+		  "Propietario:" +
+		  partida.owner +
+		  "</span>" +
+		  "<button id='btnUP' data-value='" +
+		  partida.codigo +
+		  "'" +
+		  " class='btn btn-secondary mb-2 mr-sm-2 m-2'>Unir a Partida</button>" +
+		  "</li>";
+		$("#LP").append(cadena);
+  
+		$("#btnUP").on("click", function (e) {
+		  var codigo = $(this).data("value");
+		  cws.unirAPartida($.cookie("nick"), codigo);
 		});
-	}
-	this.mostrarCrearPartida=function(){
-		$('#mCP').remove();
-		let cadena= '<div class="row" id="mCP">';//'<form class="form-row needs-validation"  id="mAJ">';
-        cadena=cadena+'<div class="col">';
-        cadena=cadena+'<button id="btnCP" class="btn btn-primary mb-2 mr-sm-2">Crear partida</button>';
-        cadena=cadena+'</div>';
-        cadena=cadena+'</div>';
-        $('#crearPartida').append(cadena);
-        $("#btnCP").on("click",function(e){		
-			$("#mCP").remove();
-			$('#mLP').remove();
-			//rest.crearPartida();
-			cws.crearPartida();
-		});
-	}
-	this.mostrarCodigo=function(codigo){
-		let cadena="Código de la partida: "+codigo;
-		$('#codigo').append(cadena);
-	}
-	this.mostrarListaDePartidas=function(lista){
-		$('#mLP').remove();
-		let cadena="<div id='mLP'>";		
-		cadena=cadena+'<ul class="list-group">';
-		for(i=0;i<lista.length;i++){
-		  cadena = cadena+'<li class="list-group-item">'+lista[i].codigo+' propietario: '+lista[i].owner+'</li>';
-		}
-		cadena=cadena+"</ul>";
-		cadena=cadena+"</div>"
-		$('#listaPartidas').append(cadena);
-		
-	}
-	this.mostrarListaDePartidasDisponibles=function(lista){
-		$('#mLP').remove();
-		let cadena="<div class='row' id='mLP'>";
-		cadena=cadena+"<div class='col'>";
-		cadena=cadena+"<h2>Lista de partidas disponibles</h2>";
-		//cadena=cadena+'<button id="btnAL" class="btn btn-primary mb-2 mr-sm-2">Actualizar</button>';
-		cadena=cadena+'<ul class="list-group">';
-		for(i=0;i<lista.length;i++){
-		  cadena = cadena+'<li class="list-group-item"><a href="#" value="'+lista[i].codigo+'"> Nick propietario: '+lista[i].owner+'</a></li>';
-		}
-		cadena=cadena+"</ul>";
-		cadena=cadena+"</div></div>"
-		$('#listaPartidas').append(cadena);
-
-		$(".list-group a").click(function(){
-	        codigo=$(this).attr("value");
-   	        console.log(codigo);
-	        if (codigo){
-	            $('#mLP').remove();
-	            $('#mCP').remove();
-	            //rest.unirseAPartida(codigo);
-	            cws.unirseAPartida(codigo);
-	        }
-	    });		
-	    $("#btnAL").on("click",function(e){		
-			rest.obtenerListaPartidasDisponibles();
-		})
-	}
-	this.mostrarModal=function(msg){
-		$('#mM').remove();
-		var cadena="<p id='mM'>"+msg+"</p>";
-		$('#contenidoModal').append(cadena);
-		$('#miModal').modal("show");
-	}
-}
+	  }
+	};
+  
+	this.mostrarModal = function (msg) {
+	  $("#mM").remove();
+	  var cadena = "<p id='mM'>" + msg + "</p>";
+	  $("#contenidoModal").html(cadena);
+	  $("#miModal").modal("show");
+	};
+  
+	this.limpiarPantalla = function () {
+	  $("#mCP").remove();
+	  $("#cP").remove();
+	  $("#mLP").remove();
+	  $("#btnLO").remove();
+	  $("#btnAP").remove();
+	  $("#mHin").remove();
+	  $("#mAU").remove();
+	  $("#LP").html("");
+	};
+  }
+  
