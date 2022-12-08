acf6456681f0f9a378c98b20b4008a6b6d71972d
Heisenth <Heisenth@gmail.com>
2022-12-08 14:13:35 +0100

    Cambios css y tablero

--- cliente/tablero.js
+++ cliente/tablero.js
@@ -1,241 +1,162 @@
-function Tablero(size) {
-  this.size = size;
-  this.nombreBarco;
-  this.orientacion = "Horizontal";
+function Tablero() {
   this.placingOnGrid = false;
-  this.flota = [];
-
-  this.anadirCallbacksBarcos = function () {
-    var playerRoster = document
-      .querySelector(".fleet-roster")
-      .querySelectorAll("li");
-    for (var i = 0; i < playerRoster.length; i++) {
-      playerRoster[i].self = this;
-      playerRoster[i].addEventListener("click", this.rosterListener, false);
-    }
-    var computerCells = document.querySelector(".computer-player").childNodes;
-    for (var j = 0; j < computerCells.length; j++) {
-      computerCells[j].self = this;
-      computerCells[j].addEventListener("click", this.shootListener, false);
-    }
-  };
-
-  this.mostrar = function (si) {
-    let listaBarcos = document.getElementById("lista-barcos");
-    let html = "";
-
-    let id = 0;
-    for (const barco of this.flota) {
-      html += `<li id=${id}>b${barco.tamano}</li>`;
-      id++;
-    }
-
-    listaBarcos.innerHTML = html;
-    this.anadirCallbacksBarcos();
-
-    let x = document.getElementById("tablero");
-    if (si) {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  };
+  this.nombreBarco = undefined;
+  this.flota;
+ 
   this.ini = function () {
-    const botonLimpiarTablero = document.querySelector("#limpiarTablero");
-    botonLimpiarTablero.self = this;
-    botonLimpiarTablero.addEventListener("click", this.limpiarTablero, false);
+      
+      var humanCells = document.querySelector('.human-player').childNodes;
+      for (var k = 0; k < humanCells.length; k++) {
+          humanCells[k].self = this;
+          humanCells[k].addEventListener('click', this.placementListener, false);
+      }
+      var playerRoster = document.querySelector('.fleet-roster').querySelectorAll('li');
 
-    const botonCambiarOrientacion = document.querySelector(
-      "#cambiarOrientacion"
-    );
-    botonCambiarOrientacion.self = this;
-    botonCambiarOrientacion.addEventListener(
-      "click",
-      this.orientacionListener,
-      false
-    );
+      for (var i = 0; i < playerRoster.length; i++) {
+          playerRoster[i].self = this;
+          playerRoster[i].addEventListener('click', this.rosterListener, false);
+      }
 
-    const botondesplegarBarcos = document.querySelector("#desplegarBarcos");
-    botondesplegarBarcos.self = this;
-    botondesplegarBarcos.addEventListener(
-      "click",
-      this.desplegarListener,
-      false
-    );
-
-    var humanCells = document.querySelector(".human-player").childNodes;
-    for (var k = 0; k < humanCells.length; k++) {
-      humanCells[k].self = this;
-      humanCells[k].addEventListener("click", this.placementListener, false);
-      //humanCells[k].addEventListener('mouseover', this.placementMouseover, false);
-      //humanCells[k].addEventListener('mouseout', this.placementMouseout, false);
-    }
-    this.anadirCallbacksBarcos();
-  };
-
-  this.orientacionListener = function (e) {
-    self = e.target.self;
-    self.cambiarOrientacion();
-    $("#cambiarOrientacion").text("Orientación: " + self.orientacion);
-  };
-
-  this.cambiarOrientacion = () => {
-    if (this.orientacion == "Horizontal") {
-      this.orientacion = "Vertical";
-    } else {
-      this.orientacion = "Horizontal";
-    }
-  };
-
-  this.limpiarTablero = () => {
-    cws.limpiarTablero();
-  };
-
-  this.desplegarListener = () => {
-    cws.barcosDesplegados();
-  };
-
-  this.placementListener = function (e) {
-    self = e.target.self;
-    if (self.placingOnGrid) {
-      // Extract coordinates from event listener
-      var x = parseInt(e.target.getAttribute("data-x"), 10);
-      var y = parseInt(e.target.getAttribute("data-y"), 10);
-
-      // Don't screw up the direction if the user tries to place again.
-      self.colocarBarco(x, y, self.nombreBarco, self.orientacion);
-    }
-  };
-  this.endPlacing = function (shipType) {
-    const shipPlace = document.getElementById(shipType);
-    shipPlace.setAttribute("class", "placed");
-    shipPlace.placingOnGrid = false;
-  };
-
-  this.resumeReplacement = function () {
-    const shipsPlace = document
-      .querySelector(".fleet-roster")
-      .querySelectorAll("li");
-
-    for (var i = 0; i < shipsPlace.length; i++) {
-      var shipPlace = shipsPlace[i];
-      shipPlace.setAttribute("class", "");
-      shipPlace.placingOnGrid = true;
-    }
-  };
-
+      var computerCells = document.querySelector('.computer-player').childNodes;
+      for (var j = 0; j < computerCells.length; j++) {
+          computerCells[j].self = this;
+          computerCells[j].addEventListener('click', this.shootListener, false);
+      }
+  }
   this.rosterListener = function (e) {
-    var self = e.target.self;
-    var cli = this;
-    // Remove all classes of 'placing' from the fleet roster first
-    var roster = document.querySelectorAll(".fleet-roster li");
-    for (var i = 0; i < roster.length; i++) {
-      var classes = roster[i].getAttribute("class") || "";
-      classes = classes.replace("placing", "");
-      roster[i].setAttribute("class", classes);
-    }
+      var self = e.target.self;
+      var roster = document.querySelectorAll('.fleet-roster li');
 
-    // Set the class of the target ship to 'placing'
-    self.nombreBarco = e.target.getAttribute("id");
-    document.getElementById(self.nombreBarco).setAttribute("class", "placing");
-    //Game.placeShipDirection = parseInt(document.getElementById('rotate-button').getAttribute('data-direction'), 10);
-    self.placingOnGrid = true;
-  };
-  this.colocarBarco = function (x, y, nombre, orientacion) {
-    //comprobar límites
-    console.log("Colocando barco: " + x + "-" + y + " " + nombre);
-    idBarco = parseInt(nombre);
-    cws.colocarBarco(idBarco, x, y, orientacion);
-
-    return true;
-  };
-
-  this.barcoColocado = function (barco, x, y, orientacion) {
-    if (orientacion.toLowerCase() === "horizontal") {
-      for (var i = 0; i < this.flota[barco].tamano; i++) {
-        console.log("x: " + (x + i) + " y:" + y);
-        this.updateCell(x + i, y, "ship", "human-player");
+      for (var i = 0; i < roster.length; i++) {
+          var classes = roster[i].getAttribute('class') || '';
+          classes = classes.replace('placing', '');
+          roster[i].setAttribute('class', classes);
       }
-    } else if (orientacion.toLowerCase() === "vertical") {
-      for (var j = 0; j < this.flota[barco].tamano; j++) {
-        console.log("x: " + x + " y:" + (y + j));
-        this.updateCell(x, y + j, "ship", "human-player");
-      }
-    }
 
-    self.endPlacing(barco);
+      self.nombreBarco = e.target.getAttribute('id');
+      document.getElementById(self.nombreBarco).setAttribute('class', 'placing');
+      //Game.placeShipDirection = parseInt(document.getElementById('rotate-button').getAttribute('data-direction'), 10);
+      self.placingOnGrid = true;
+  };
+  // Creates click event listeners on the human player's grid to handle
+  this.placementListener = function (e) {
+      var self = e.target.self;
+      if (self.placingOnGrid) {
+          // Extract coordinates from event listener
+          var x = parseInt(e.target.getAttribute('data-x'), 10);
+          var y = parseInt(e.target.getAttribute('data-y'), 10);
+          //console.log("Barco: " + self.nombreBarco + " x: " + x + " y: " + y);
+          // Don't screw up the direction if the user tries to place again.
+          //var successful = 
+          self.colocarBarco(self.nombreBarco, x, y);
+          //if (successful) {
+          //     // Done placing this ship
+
+          //self.endPlacing(Game.placeShipType);
+          //self.placingOnGrid = false;
+
+          /*if (self.areAllShipsPlaced()) {
+              var el = document.getElementById('rotate-button');
+              el.addEventListener(transitionEndEventName(), (function () {
+                  el.setAttribute('class', 'hidden');
+                  if (gameTutorial.showTutorial) {
+                      document.getElementById('start-game').setAttribute('class', 'highlight');
+                  } else {
+                      document.getElementById('start-game').removeAttribute('class');
+                  }
+              }), false);
+              el.setAttribute('class', 'invisible');
+          } */
+          //}
+      }
+  };
+
+  this.endPlacing = function (shipType) {
+      document.getElementById(shipType).setAttribute('class', 'placed');
+
+      // Wipe out the variable when you're done with it
+      //Game.placeShipDirection = null;
+      this.nombreBarco = '';
+      //Game.placeShipCoords = [];
+  };
+  this.colocarBarco = function (nombre, x, y) {
+      //console.log("Barco: " + nombre + " x: " + x + " y: " + y);
+      cws.colocarBarco(nombre, x, y);
+      cws.barcosDesplegados();
+      //return true;
+  }
+
+  this.disparo = function (x, y) {
+      cws.disparar(x, y);
+  }
+  this.puedesDisparar = function (estado, x, y, targetPlayer) {
+
+      if (estado == 'agua') {
+          this.updateCell(x, y, 'agua', targetPlayer);
+      } else if (estado == 'tocado') {
+          this.updateCell(x, y, 'tocado', targetPlayer);
+
+      } else {
+          this.updateCell(x, y, 'hundido', targetPlayer);
+      }
+      this.placingOnGrid = false;
   };
 
   this.shootListener = function (e) {
-    var x = parseInt(e.target.getAttribute("data-x"), 10);
-    var y = parseInt(e.target.getAttribute("data-y"), 10);
-    console.log("disparo x: " + x + " y: " + y);
-    cws.disparar(x, y);
-  };
-  this.updateCell = function (x, y, type, target) {
-    var player = target; //'human-player';
-    // if (targetPlayer === CONST.HUMAN_PLAYER) {
-    // 	player = 'human-player';
-    // } else if (targetPlayer === CONST.COMPUTER_PLAYER) {
-    // 	player = 'computer-player';
-    // } else {
-    // 	// Should never be called
-    // 	console.log("There was an error trying to find the correct player's grid");
-    // }
+      var self = e.target.self;
+      // Extract coordinates from event listener
+      var x = parseInt(e.target.getAttribute('data-x'), 10);
+      var y = parseInt(e.target.getAttribute('data-y'), 10);
 
-    // switch (type) {
-    // 	case CONST.CSS_TYPE_EMPTY:
-    // 		this.cells[x][y] = CONST.TYPE_EMPTY;
-    // 		break;
-    // 	case CONST.CSS_TYPE_SHIP:
-    // 		this.cells[x][y] = CONST.TYPE_SHIP;
-    // 		break;
-    // 	case CONST.CSS_TYPE_MISS:
-    // 		this.cells[x][y] = CONST.TYPE_MISS;
-    // 		break;
-    // 	case CONST.CSS_TYPE_HIT:
-    // 		this.cells[x][y] = CONST.TYPE_HIT;
-    // 		break;
-    // 	case CONST.CSS_TYPE_SUNK:
-    // 		this.cells[x][y] = CONST.TYPE_SUNK;
-    // 		break;
-    // 	default:
-    // 		this.cells[x][y] = CONST.TYPE_EMPTY;
-    // 		break;
-    // }
-    var classes = ["grid-cell", "grid-cell-" + x + "-" + y, "grid-" + type];
-    document
-      .querySelector("." + player + " .grid-cell-" + x + "-" + y)
-      .setAttribute("class", classes.join(" "));
-  };
-  this.createGrid = function () {
-    var gridDiv = document.querySelectorAll(".grid");
 
-    for (var grid = 0; grid < gridDiv.length; grid++) {
-      //gridDiv[grid].removeChild(gridDiv[grid].querySelector('.no-js')); // Removes the no-js warning
-      let myNode = gridDiv[grid];
-      while (myNode.lastElementChild) {
-        myNode.removeChild(myNode.lastElementChild);
+      self.disparo(x, y);
+  };
+  this.mostrarTablero = function () {
+
+  }
+  this.puedesColocarBarco = function (barco, x, y, desplegados) {
+
+      for (i = 0; i < barco.tam; i++) {
+          this.updateCell(x + i, y, 'ship', 'human-player');
       }
-      for (var i = 0; i < this.size; i++) {
-        for (var j = 0; j < this.size; j++) {
-          var el = document.createElement("div");
-          el.setAttribute("data-x", j);
-          el.setAttribute("data-y", i);
-          el.setAttribute("class", "grid-cell grid-cell-" + j + "-" + i);
-          gridDiv[grid].appendChild(el);
-        }
-      }
-    }
-    this.ini();
-  };
+      this.placingOnGrid = false;
+      this.endPlacing(barco.nombre);
 
-  this.limpiarGrid = function () {
-    this.createGrid();
-    this.resumeReplacement();
+
+
+  }
+  // Updates the cell's CSS class based on the type passed in
+  this.updateCell = function (x, y, type, targetPlayer) {
+      var player = targetPlayer;
+      var classes = ['grid-cell', 'grid-cell-' + x + '-' + y, 'grid-' + type];
+      document.querySelector('.' + player + ' .grid-cell-' + x + '-' + y).setAttribute('class', classes.join(' '));
   };
-  this.createGrid();
-  this.mostrar(false);
+  this.crearGrid = function () {
+      var gridDiv = document.querySelectorAll('.grid');
+      for (var grid = 0; grid < gridDiv.length; grid++) {
+          //gridDiv[grid].removeChild(gridDiv[grid].querySelector('.no-js')); // Removes the no-js warning
+          for (var i = 0; i < 10; i++) {
+              for (var j = 0; j < 10; j++) {
+                  var el = document.createElement('div');
+                  el.setAttribute('data-x', j);
+                  el.setAttribute('data-y', i);
+                  el.setAttribute('class', 'grid-cell grid-cell-' + j + '-' + i);
+                  gridDiv[grid].appendChild(el);
+              }
+          }
+      }
+  }
+  this.mostrarTablero = function(flag){
+      let tab = document.getElementById("tablero");
+      if (flag) {
+          tab.style.display = "block";
+      } else {
+          tab.style.display = "none";
+      }
+  };
+  //Constructor
+  this.crearGrid();
+  this.ini();
 
 
 }
